b875a33d75070880299174312122e78dbc9937da
nileshleve <nilesh.leve04@gmail.com>
2019-10-22 16:48:36 +0000

    Corrected unit test cases

--- spark/projects/apache-log-parsing_sbt/src/test/scala/com/cloudxlab/logparsing/log-parser-test.scala
+++ spark/projects/apache-log-parsing_sbt/src/test/scala/com/cloudxlab/logparsing/log-parser-test.scala
@@ -5,14 +5,14 @@ import org.scalatest.FlatSpec
 class LogParserSpec extends FlatSpec {
 
   "extractIP" should "Extract IP Address" in {
-    val utils = new Utils
+    val utils = new IPUtils
     var line = "121.242.40.10 - - [03/Aug/2015:06:30:52 -0400] \"POST /mod_pagespeed_beacon?url=http%3A%2F%2Fwww.knowbigdata.com%2Fpage%2Fabout-us HTTP/1.1\" 204 206 \"http://www.knowbigdata.com/page/about-us\" \"Mozilla/5.0 (Windows NT 6.3; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0\""
     var ip = utils.extractIP(line)
     assert(ip == "121.242.40.10")
   }
 
   "containsIP" should "Check if IP exists in the log line" in {
-    val utils = new Utils
+    val utils = new IPUtils
     var line1 = "121.242.40.10 - - [03/Aug/2015:06:30:52 -0400] \"POST /mod_pagespeed_beacon?url=http%3A%2F%2Fwww.knowbigdata.com%2Fpage%2Fabout-us HTTP/1.1\" 204 206 \"http://www.knowbigdata.com/page/about-us\" \"Mozilla/5.0 (Windows NT 6.3; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0\""
 
     assert(utils.containsIP(line1))
